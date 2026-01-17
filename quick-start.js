25617b78c70065dc0eb247aa432afc43ab1e689c
DinoIjoNPC <bielnpcbaik@gmail.com>
2026-01-17 18:10:14 +0800

    Create quick-start.js

---
+++ quick-start.js
@@ -0,0 +1,21 @@
+// quick-start.js - Copy and paste into browser console
+const quickStart = {
+    setup() {
+        console.log('OXYLUS_PROTOCOL Quick Setup');
+        console.log('1. Deploy files to hosting');
+        console.log('2. Access URL in browser');
+        console.log('3. Password: DinoProtocol');
+        console.log('4. Use commands below:\n');
+        
+        console.log('// DDoS Example:');
+        console.log('OXYLUS.ddos.target("target.com", 80, "hard", 30)');
+        
+        console.log('\n// NGL Spam Example:');
+        console.log('OXYLUS.ngl.spam("ngl.link/victim", "Test message", 20, 1500)');
+        
+        console.log('\n// Stop all:');
+        console.log('OXYLUS.stop.all()');
+    }
+};
+
+quickStart.setup();
